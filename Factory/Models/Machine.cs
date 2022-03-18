a4471b66c02b7e45a45f4f83fdda0ec71a404795
hmcvay <hayleymcvay8@gmail.com>
2022-03-18 09:59:34 -0700

    Update all models and change all naming conventions from the template to current project.

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<MachineEngineer>();
+    }
+    public int MachineId {get; set; }
+    public string Type { get; set; }
+    public virtual ICollection<MachineEngineer> JoinEntities { get; set; }
+  }
+}
